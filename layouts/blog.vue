168232c80f85b00ac57e322163cecf9ee593d8e0
Sahil <sahil@kokamkar.com>
2024-04-06 14:53:36 +0000

    fix: full screen dark mode

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <SkipContent />
-    <div class="bg-white dark:bg-slate-900">
+    <div class="bg-white dark:bg-slate-900 h-screen">
       <div
         class="max-w-2xl px-4 py-10 m-auto bg-slate-50 dark:bg-gray-800 sm:px-8 sm:shadow ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg"
       >
